51c83b9c2018fc47388c088fb3780ba489c3b48d
Axel <axosar@gmail.com>
2019-06-12 13:51:53 -0500

    Creating Pages
    Creating routing pages

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
 import Page1 from './components/Page1';
+import Page2 from './components/Page2';
+import Page3 from './components/Page3';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-      </header>
-      <Page1 />
-      <Page2 />
-      <Page3 />
-    </div>
-  );
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      route: 'page1'
+    }
+  }
+
+  onRouteChange = (route) => {
+    console.log(route);
+    this.setState({route: route})
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Page1 onRouteChange={this.onRouteChange} />
+        <Page2 onRouteChange={this.onRouteChange} />
+        <Page3 onRouteChange={this.onRouteChange} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Page2.js
+++ src/components/Page2.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import logo from '../logo.svg';
 
-const Page1 = ({ onRouteChange }) =>
+const Page2 = ({ onRouteChange }) =>
   <div className="App">
     <header className="App-header">
       <img src={logo} className="App-logo" alt="logo" />
     </header>
-    <button onClick={() => onRouteChange('Page2')}>Page2</button>
+    <button onClick={() => onRouteChange('Page1')}>Page1</button>
     <button onClick={() => onRouteChange('Page3')}>Page3</button>
   </div>
 
-export default Page1;
+export default Page2;
